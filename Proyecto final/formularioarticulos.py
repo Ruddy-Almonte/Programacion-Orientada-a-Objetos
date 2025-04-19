ae2c0ecd7b28c1d4ca018b979a737320c33ddd10
Ruddy.Almonte <ran.djc2@gmail.com>
2025-04-19 11:11:47 -0400

    proyectofinal

---
+++ Proyecto final/formularioarticulos.py
@@ -0,0 +1,9 @@
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mb
+from tkinter import scrolledtext as st
+
+#importamos el modulo articulos para la conexion con la base de datos
+
+import articulos as art
