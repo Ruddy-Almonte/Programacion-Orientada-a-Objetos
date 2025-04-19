8729fb8b424f4d34b72c7c5cb47cdcbeecf940df
Ruddy.Almonte <ran.djc2@gmail.com>
2025-04-19 11:41:37 -0400

    proyectofinal

--- Proyecto final/formularioarticulos.py
+++ Proyecto final/formularioarticulos.py
@@ -3,7 +3,10 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox as mb
 from tkinter import scrolledtext as st
+import articulos
 
-#importamos el modulo articulos para la conexion con la base de datos
 
-import articulos as art
+class formularioarticulos:
+    def __init__(self):
+        self.articulo1=articulos.Articulo()
+        
